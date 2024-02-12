eee45531506755f04b71c9d7df6af1187e23d1e3
Kaskaya <recep.tkaskaya@gmail.com>
2024-02-12 22:07:54 +0300

    initialized project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
+import { Montserrat } from "next/font/google";
+
+const montserrat = Montserrat({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={montserrat.className}>
+      <Head>
+        <title>Movie Sphere</title>
+        <meta name="description" content="A movie site named movie sphere" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
 }
