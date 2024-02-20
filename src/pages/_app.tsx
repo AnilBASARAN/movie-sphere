032baf2920f6a4588e27bc5e006b549468a9f7b9
Kaskaya <recep.tkaskaya@gmail.com>
2024-02-20 20:34:53 +0300

    corrected global layout and provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,8 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import { Montserrat } from "next/font/google";
+import Layout from "@/components/Layout";
+import { MainProvider } from "@/components/Context/context";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
@@ -14,7 +16,11 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Component {...pageProps} />
+      <MainProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </MainProvider>
     </div>
   );
 }
