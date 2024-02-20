7064d8c6b0735980f80a6740f9a337267b8977e4
Kaskaya <recep.tkaskaya@gmail.com>
2024-02-20 20:40:41 +0300

    move context files to its own folder

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,134 +1,13 @@
-/* import Layout from "@/components/Layout"; */
-import { MainContext } from "@/components/context"; // React Context Api Structure
-
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
+import { MainContext } from "@/components/Context/context"; // React Context Api Structure
+import { useContext } from "react";
 import Link from "next/link";
-import { useState, useEffect } from "react";
-import {
-  API_URL,
-  IMG_URL,
-  searchURL,
-  getMovies,
-  Movie,
-  genres,
-  GENRE_URL,
-} from "@/pages/api/api";
 
 const Home: React.FC = () => {
-  const [movies, setMovies] = useState<Movie>();
-  const [searchTerm, setSearchTerm] = useState<string>("");
-  const [count, setCount] = useState<number>(1);
-  const [isOpen, setIsOpen] = useState(false);
-  const [activeItem, setActiveItem] = useState<string>("");
-
-  // Toggle Functionality
-  const handleToggle = () => {
-    setIsOpen(!isOpen);
-  };
-
-  // Get Animations
-  const handleAnimations = () => {
-    handleMovies({
-      id: 16,
-      name: "Animation",
-    });
-    setCount(1);
-  };
-
-  // Search Functionality
-  const handleSearch = (e: any) => {
-    handleMovies(e);
-    setCount(1);
-  };
-
-  // Get Genres
-  const handleGenres = (e: any) => {
-    handleMovies(e);
-    setCount(1);
-  };
-
-  // Get All The Data Based On Their Type
-  const handleMovies = async (genre: any) => {
-    if (genre.preventDefault) {
-      genre.preventDefault();
-      setActiveItem(genre);
-      const searchResults = await getMovies(
-        `${searchURL}&query=${searchTerm}&page=${count}`
-      );
-      setMovies(searchResults);
-    } else {
-      if (genre?.name) {
-        setActiveItem(genre);
-        const data = await getMovies(`${GENRE_URL}${genre.id}&page=${count}`);
-        setMovies(data);
-      } else if (genre) {
-        setActiveItem(genre);
-        const searchResults = await getMovies(
-          `${searchURL}&query=${searchTerm}&page=${count}`
-        );
-        setMovies(searchResults);
-      } else {
-        setActiveItem("");
-        const data = await getMovies(`${API_URL}&page=${count}`);
-        setMovies(data);
-      }
-    }
-  };
-
-  useEffect(() => {
-    handleMovies(activeItem);
-  }, [count]);
-
-  // Refresh page
-  const refreshPage = () => {
-    window.location.reload();
-  };
-
-  // Pagination
-  const increaseCount = () => {
-    if (movies && movies.total_pages > count) {
-      setCount((prevCount) => prevCount + 1);
-    }
-  };
-
-  const decreaseCount = () => {
-    if (count > 1) {
-      setCount((prevCount) => prevCount - 1);
-    }
-  };
-
-  // Fetching Datas To Components Via React Context Api
-  const fetchToComponents = {
-    movies,
-    setMovies,
-    searchTerm,
-    setSearchTerm,
-    count,
-    setCount,
-    isOpen,
-    setIsOpen,
-    handleToggle,
-    handleSearch,
-    API_URL,
-    IMG_URL,
-    searchURL,
-    getMovies,
-    genres,
-    GENRE_URL,
-    refreshPage,
-    handleMovies,
-    handleGenres,
-    handleAnimations,
-    activeItem,
-    setActiveItem,
-    increaseCount,
-    decreaseCount,
-  };
+  const { movies, count, setCount, IMG_URL, increaseCount, decreaseCount } =
+    useContext<any>(MainContext);
 
   return (
-    <MainContext.Provider value={fetchToComponents}>
-      <Header />
+    <>
       <div className="flex justify-center text-sm ">
         <div
           id="main"
@@ -202,8 +81,7 @@ const Home: React.FC = () => {
           </button>
         </div>
       )}
-      <Footer />
-    </MainContext.Provider>
+    </>
   );
 };
 
