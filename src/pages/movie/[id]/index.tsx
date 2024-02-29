0faa35dd13cc7688dbe13b7e7dac5bc4676d8178
Kaskaya <recep.tkaskaya@gmail.com>
2024-02-29 03:44:39 +0300

    installed react player for movie video

--- src/pages/movie/[id]/index.tsx
+++ src/pages/movie/[id]/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
+import ReactPlayer from "react-player";
 
 import { getMovies, BASE_URL, API_KEY, IMG_URL } from "@/pages/api/api";
 
@@ -7,11 +8,12 @@ const Movies: React.FC = () => {
   const router = useRouter();
   const { id } = router.query;
   const [movies, setMovies] = useState<any>("");
+  const [isOkay, setIsOkay] = useState<any>(false);
 
   // Get Movies By Id
   const handleMovieById = async () => {
     const data = await getMovies(
-      `${BASE_URL}/movie/${id}?language=en-US&${API_KEY}`
+      `${BASE_URL}/movie/${id}?&append_to_response=videos&language=en-US&${API_KEY}`
     );
     setMovies(data);
   };
@@ -21,16 +23,30 @@ const Movies: React.FC = () => {
     if (id) {
       handleMovieById();
     }
+    setIsOkay(true);
   }, [id]);
 
   return (
-    <>
-      <h1 className="text-5xl m-10 text-center bg-red-500 p-8">
+    <div>
+      {/* <h1 className="text-5xl m-10 text-center bg-red-500 p-8">
         Under Construction
-      </h1>
+      </h1> */}
+
       <img src={`${IMG_URL + movies.poster_path}`} alt={movies.title} />
       <div>{movies.original_title}</div>
-    </>
+      {isOkay && (
+        <div>
+          <ReactPlayer
+            url={`https://www.youtube.com/watch?v=${
+              movies.videos?.results[movies.videos?.results.length - 1].key
+            }`}
+            controls
+            width="100%"
+            height="auto"
+          />{" "}
+        </div>
+      )}
+    </div>
   );
 };
 
