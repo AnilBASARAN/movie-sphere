853ae676ad62ac6ff42ddd95241771e230de6e4f
Kaskaya <recep.tkaskaya@gmail.com>
2024-04-21 12:29:56 +0300

    Stars below user movies

---
+++ src/components/Stars/index.tsx
@@ -0,0 +1,40 @@
+// components/Rating.tsx
+
+import React from "react";
+
+const Stars: React.FC<{ rating: number }> = ({ rating }) => {
+  const stars = [];
+  const filledStars = Math.floor(rating);
+  const remainder = rating - filledStars;
+
+  for (let i = 1; i <= 5; i++) {
+    if (i <= filledStars) {
+      stars.push(
+        <span key={i} className="text-yellow-400 text-lg">
+          &#9733;
+        </span>
+      );
+    } else if (remainder > 0 && i === filledStars + 1) {
+      stars.push(
+        <span key={i} className="relative inline-block">
+          <span
+            className="absolute"
+            style={{ width: `${remainder * 100}%`, overflow: "hidden" }}
+          >
+            <span className="text-yellow-400 text-lg">&#9733;</span>
+          </span>
+          <span className="text-gray-500 text-lg">&#9733;</span>
+        </span>
+      );
+    } else {
+      stars.push(
+        <span key={i} className="text-gray-500 text-lg">
+          &#9733;
+        </span>
+      );
+    }
+  }
+  return <div>{stars}</div>;
+};
+
+export default Stars;
