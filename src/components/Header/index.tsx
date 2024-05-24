65c738ffdb2ac229982db715c6a8c765c218a97e
Kaskaya <recep.tkaskaya@gmail.com>
2024-05-24 16:32:23 +0300

    enhancements

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -171,29 +171,28 @@ function Header() {
                 onMouseLeave={() => setIsProfileOpen(false)}
                 className=" h-16 min-w-24 max-w-36 font-bold truncate "
               >
-                {auth?.currentUser?.email}
+                {auth?.currentUser?.displayName || auth?.currentUser?.email}
               </button>
-              <div
-                className={isProfileOpen ? "absolute top-full w-full     " : ""}
-              >
+              <div className={isProfileOpen ? "absolute top-full w-full" : ""}>
                 {isProfileOpen && (
                   <div
                     className="  bg-slate-800 overflow-hidden shadow-xl rounded-b-md "
                     onMouseEnter={() => setIsProfileOpen(true)}
                     onMouseLeave={() => setIsProfileOpen(false)}
                   >
-                    <a
-                      href="#"
-                      className="block px-4 py-2 text-gray-200 border-t-[1px] border-gray-600 hover:text-green-400 hover:bg-slate-700  "
-                    >
-                      Settings
-                    </a>
                     <Link
                       href={`/user/${auth?.currentUser?.email}/movies`}
                       className="block px-4 py-2 text-gray-200 border-t-[1px] border-gray-600 hover:text-green-400 hover:bg-slate-700 "
                     >
-                      User
+                      Your Collection
                     </Link>
+                    {/*   <a
+                      href="#"
+                      className="block px-4 py-2 text-gray-200 border-t-[1px] border-gray-600 hover:text-green-400 hover:bg-slate-700  "
+                    >
+                      Profile
+                    </a> */}
+
                     <a
                       onClick={logout}
                       href="/"
