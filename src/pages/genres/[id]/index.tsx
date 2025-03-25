3fcbab0ecbf8b20fe3824336c4097f0759010c3a
AnilBASARAN <nlbsrn@gmail.com>
2025-03-25 23:45:59 +0300

    added some functionality

--- src/pages/genres/[id]/index.tsx
+++ src/pages/genres/[id]/index.tsx
@@ -3,8 +3,10 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import { GENRE_URL, getMovies, IMG_URL, genres } from "@/pages/api/api";
 import Pagination from "@/pages/api/methods/pagination";
+import Navigator from "@/components/Header/Navigator";
 
 const Genres: React.FC = () => {
+  const [selectedFilm,setSelectedFilm] = useState("Action");
   const router = useRouter();
   const { query }: any = router;
   const [currentPage, setCurrentPage] = useState<number>(
@@ -29,6 +31,81 @@ const Genres: React.FC = () => {
     }
   };
 
+  const allGenres = [
+    {
+      id: 28,
+      name: "Action",
+    },
+    {
+      id: 12,
+      name: "Adventure",
+    },
+    {
+      id: 16,
+      name: "Animation",
+    },
+    {
+      id: 35,
+      name: "Comedy",
+    },
+    {
+      id: 80,
+      name: "Crime",
+    },
+    {
+      id: 99,
+      name: "Documentary",
+    },
+    {
+      id: 18,
+      name: "Drama",
+    },
+    {
+      id: 10751,
+      name: "Family",
+    },
+    {
+      id: 14,
+      name: "Fantasy",
+    },
+    {
+      id: 36,
+      name: "History",
+    },
+    {
+      id: 27,
+      name: "Horror",
+    },
+    {
+      id: 10402,
+      name: "Music",
+    },
+    {
+      id: 9648,
+      name: "Mystery",
+    },
+    {
+      id: 10749,
+      name: "Romance",
+    },
+    {
+      id: 878,
+      name: "Science Fiction",
+    },
+    {
+      id: 53,
+      name: "Thriller",
+    },
+    {
+      id: 10752,
+      name: "War",
+    },
+    {
+      id: 37,
+      name: "Western",
+    },
+  ];
+
   // Get Pages From URL
   useEffect(() => {
     if (query.page) {
@@ -41,7 +118,20 @@ const Genres: React.FC = () => {
 
   return (
     <>
-      <div className="flex justify-center text-sm min-h-[78vh] ">
+      <div className="flex flex-col items-center justify-center text-sm min-h-[78vh] ">
+
+{/*       <div className="flex flex-wrap gap-2">
+  {allGenres.map(({ id, name }, index) => (
+    <div
+      key={id || index}
+      className="badge mx-1 my-1 badge-secondary cursor-pointer"
+      onClick={() => setSelectedFilm(name)}
+    >
+      {name}
+    </div>
+  ))}
+</div> */}
+  <Navigator selectedTab={selectedFilm} setSelectedTab={setSelectedFilm} />
         <div
           id="main"
           className="grid grid-cols-4  lg:grid-cols-5 justify-center w-full max-w-[1000px] gap-3 m-4"
